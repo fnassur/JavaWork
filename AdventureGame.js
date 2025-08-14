97bdd18425b1f60c7a3c8df04e23d8648a1e33ce
Faiz Nassur <faiz.ali.nassur.93@gmail.com>
2025-08-14 18:25:05 -0500

    New Changes to file

--- AdventureGame.js
+++ AdventureGame.js
@@ -1,5 +1,17 @@
 //AdventureGame script
 
-const readline = require("readline - sync");
+const readline = require("readline-sync");
 
 let playerName = "";
+let playerHealth = 100;
+let playerGold = 20;
+let currentLocation = "village";
+let gameRunning = true;
+let inventory = [];
+
+console.log("Enter Player Name: ");
+
+playerName = readline.question();
+
+console.log("Welcome player: " & playerName);
+console.log("Your current gold is: " & playerGold);
