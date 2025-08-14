f0a2977409496d6afc4ebe7981a597e9046d3737
Faiz Nassur <faiz.ali.nassur.93@gmail.com>
2025-08-14 04:01:07 -0500

    Initial commit - character stats setup

---
+++ calculations.js
@@ -0,0 +1,7 @@
+//Basica Calculations:
+
+//Addition:
+
+let sum = 5 + 3;
+
+console.log(sum)
---
+++ shoppingCart.js
@@ -0,0 +1,10 @@
+// Items in a shopping cart
+
+// Item prices:
+
+let item1Price = 20;
+let item2Price = 10;
+
+let totalPrice = item1Price + item2Price;
+
+console.log("Total Price = $", totalPrice);
